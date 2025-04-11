7def1d2fe3c6ae9757465ad1a37a032ac2f4e059
drestanto <dyas@live.com>
2025-04-11 12:50:31 +0700

    edit setup for deployment

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     version="0.1.0",  # Initial version
     packages=find_packages(),  # Automatically find packages in the directory
     install_requires=[],  # List of dependencies (empty for now, add as needed)
-    author="Your Name",  # Replace with your name
-    author_email="your_email@example.com",  # Replace with your email
+    author="Drestanto Muhammad Dyasputro",
+    author_email="dyas@live.com",
     description="A library for managing hierarchical numerical data structures",  # Short description
-    long_description=open('README.md').read(),  # Read long description from README.md
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",  # Format of the long description
-    url="https://github.com/yourusername/fractal-forest",  # Link to your project’s repository
+    url="https://github.com/drestanto/fractal-forest",  # Link to your project’s repository
     license="MIT",  # Choose your license (MIT in this case)
     classifiers=[  # Classifiers help users find your project
         "Programming Language :: Python :: 3",  # Python 3 compatibility
