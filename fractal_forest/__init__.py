d759e3af7be7bb2e425891b93c1f75ee114cafee
drestanto <dyas@live.com>
2025-04-11 12:43:09 +0700

    refactor modules

---
+++ fractal_forest/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+
+# Importing the necessary classes from the fractal_forest module
+from .node import Node
+from .tree import Tree
+
+# You can also include any helper functions or variables that are meant to be part of the public API
+__all__ = ["Node", "Tree"]
