5b21c0ec56d762786fb5780654c7bab51cb21334
drestanto <dyas@live.com>
2025-04-11 12:49:09 +0700

    add version

--- fractal_forest/__init__.py
+++ fractal_forest/__init__.py
@@ -1,5 +1,11 @@
 # __init__.py
 
+# Metadata
+__version__ = '0.1.0'
+__author__ = 'Drestanto Muhammad Dyasputro'
+__author_email__ = 'dyas@live.com'
+__license__ = 'MIT'
+
 # Importing the necessary classes from the fractal_forest module
 from .node import Node
 from .tree import Tree
